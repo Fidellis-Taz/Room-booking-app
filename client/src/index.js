ffce29272f1e074a306d1bf371f8e1c5cd4521e8
unknown <fidellis.dango@gmail.com>
2021-10-10 03:18:21 -0700

    Redux setup refactoring

--- client/src/index.js
+++ client/src/index.js
@@ -8,25 +8,12 @@ import reportWebVitals from './reportWebVitals';
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
 
 // 2. create user reducer function
-const authReducer = (state = {}, action)=>{
-  switch (action.type) {
-    case "LOGGED_IN_USER":
-      return {...state, ...action.payload}
-      
-    case "LOGOUT":
-      return action.payload; 
-    default:
-      return state;
-  }
-} 
+ 
 
 // 3. combine multiple reducers
-const rootReducer = combineReducers({
-  user: authReducer,
-});
-
 
 // 4. create redux store
 const store = createStore(rootReducer, composeWithDevTools());
